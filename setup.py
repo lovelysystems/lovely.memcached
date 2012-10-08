37cd0a0f4ee20728ed14e54e1ff7cbcb74eb3212
Alin Voinea <alin@eaudeweb.ro>
2012-10-08 12:13:59 +0300

    Preparing to release lovely.memcached 0.2.1

--- setup.py
+++ setup.py
@@ -1,31 +1,34 @@
 #!/usr/bin/env python
+""" Installer
+"""
 import os
 from setuptools import setup, find_packages
 
-# We can not include the full README.txt file since it contains
-# unicode chars within doctests which will make setuptools break.
-long_description = file('README.txt').read()
+NAME = 'lovely.memcached'
+PATH = ['src'] + NAME.split('.') + ['version.txt']
+VERSION = open(os.path.join(*PATH)).read().strip()
 
-setup (
-    name='lovely.memcached',
-    version='0',
-    author = "Lovely Systems",
-    author_email = "office@lovelysystems.com",
-    description = "A memcached client utiltiy for zope 3",
-    long_description = long_description,
-    license = "ZPL 2.1",
-    keywords = "zope3 zope memcached cache ram",
-    url = 'http://github.com/lovelysystems/lovely.memcached',
-    packages = find_packages('src'),
-    include_package_data = True,
-    package_dir = {'':'src'},
-    namespace_packages = ['lovely'],
-    extras_require = dict(test = ['z3c.configurator',
-                                  'zope.keyreference',
-                                  'zope.securitypolicy',
-                                  'zope.app.testing',
-                                  'zope.testbrowser',
-                                  ]),
+setup(
+    name=NAME,
+    version=VERSION,
+    author="Lovely Systems",
+    author_email="office@lovelysystems.com",
+    description="A memcached client utiltiy for zope 3",
+    long_description=(open("README.txt").read() + "\n" +
+                      open("CHANGES.txt").read()),
+    license="ZPL 2.1",
+    keywords="zope3 zope memcached cache ram",
+    url='http://github.com/lovelysystems/lovely.memcached',
+    packages=find_packages('src'),
+    include_package_data=True,
+    package_dir={'':'src'},
+    namespace_packages=['lovely'],
+    extras_require=dict(test = ['z3c.configurator',
+                                'zope.keyreference',
+                                'zope.securitypolicy',
+                                'zope.app.testing',
+                                'zope.testbrowser',
+                                ]),
     install_requires = ['setuptools',
                         'python-memcached',
                         'zope.site',
@@ -37,4 +40,4 @@ setup (
                         'zope.security'
                         ],
     zip_safe = False,
-    )
+)
